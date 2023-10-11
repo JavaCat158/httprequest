f74efb866530656f6c608c7abf062b916a69c5b1
Ildar <ibragimovildar229@gmail.com>
2023-10-11 19:18:29 +0300

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,62 @@
+
+
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            CloseableHttpClient httpClient = HttpClientBuilder.create()
+                    .setDefaultRequestConfig(RequestConfig.custom()
+                            .setConnectTimeout(5000)
+                            .setSocketTimeout(30000)
+                            .setRedirectsEnabled(false)
+                            .build())
+                    .build();
+
+            HttpGet request = new HttpGet("https://api.nasa.gov/planetary/apod?api_key=6G09KL8cHz0rxDtt1LylWrtqHvE5l4u94gSDX95Q");
+            CloseableHttpResponse response = httpClient.execute(request);
+
+            String jsonString = EntityUtils.toString(response.getEntity());
+
+
+            ObjectMapper mapper = new ObjectMapper();
+            NasaApi nasaResponse = mapper.readValue(jsonString, NasaApi.class);
+
+            saveJsonToFile(jsonString, "nasa.json");
+
+            String fileName = nasaResponse.getUrl().substring(nasaResponse.getUrl().lastIndexOf("/") + 1);
+
+            try (InputStream in = new URL(nasaResponse.getUrl()).openStream()) {
+                Files.copy(in, Paths.get(fileName));
+            }
+
+            response.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void saveJsonToFile(String json, String filename) {
+        try (FileWriter file = new FileWriter(filename)) {
+            file.write(json);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
---
+++ src/main/java/NasaApi.java
@@ -0,0 +1,46 @@
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.io.Serializable;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class NasaApi implements Serializable {
+    private String url;
+    private String date;
+    private String explanation;
+    private String hdurl;
+    private String media_type;
+    private String service_version;
+    private String title;
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getExplanation() {
+        return explanation;
+    }
+
+    public String getHdurl() {
+        return hdurl;
+    }
+
+    public String getMedia_type() {
+        return media_type;
+    }
+
+    public String getService_version() {
+        return service_version;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
